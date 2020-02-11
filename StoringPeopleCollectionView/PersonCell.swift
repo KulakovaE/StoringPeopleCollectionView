20c0e6d7de318003533bb7020b4368eb7e776d17
Elena Kulakova <egeorgievska89@gmail.com>
2020-02-11 20:48:37 +0100

    Designing CollectionView cells

---
+++ StoringPeopleCollectionView/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  StoringPeopleCollectionView
+//
+//  Created by Elena Kulakova on 2020-02-11.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var personImageView: UIImageView!
+    @IBOutlet weak var personName: UILabel!
+}
