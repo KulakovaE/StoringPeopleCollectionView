ecabc92821dac0a2380e6e1c24a4af052f169a02
Elena Kulakova <egeorgievska89@gmail.com>
2020-02-12 12:34:33 +0100

    Saving data with UserDefaults and Codable protocol.

--- StoringPeopleCollectionView/ViewController.swift
+++ StoringPeopleCollectionView/ViewController.swift
@@ -15,8 +15,30 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+   
+        let defaults = UserDefaults.standard
+
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            let jsonDecoder = JSONDecoder()
+
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
+        }
     }
 
+    func save() {
+       let jsonEncoder = JSONEncoder()
+        if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+            print("Failed to save people.")
+        }
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return people.count
     }
@@ -51,6 +73,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             guard let newName = alertController?.textFields?[0].text else { return }
             person.name = newName
             self?.collectionView.reloadData()
+            self?.save()
         })
 //        alertController.addAction(UIAlertAction(title: "Delete", style: .default) { [weak self] _ in
 //            self?.people.remove(at: indexPath.item)
@@ -74,6 +97,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         people.append(person)
         collectionView.reloadData()
         dismiss(animated: true)
+        self.save()
     }
     
     func getDocumentsDirectory() -> URL {
